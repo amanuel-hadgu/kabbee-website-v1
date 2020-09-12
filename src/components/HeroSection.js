fa3f4d623385c96a31f6e91a6dd631b9fb3564ef
amanuel <emahadgugm@gmail.com>
2020-09-12 04:42:19 +0300

    repo clone within main directory deleted

---
+++ src/components/HeroSection.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../App.css';
+import { Button } from './Button';
+import './HeroSection.css';
+
+function HeroSection() {
+  return (
+    <div className='hero-container'>
+      <h1>Kabbee works on Empowering Eritrean Refugees</h1>
+      <p>You want to Explore our Services?</p>
+      <div className='hero-btns'>
+        <Button
+          className='btns'
+          buttonStyle='btn--outline'
+          buttonSize='btn--large'
+        >
+          GET STARTED
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
